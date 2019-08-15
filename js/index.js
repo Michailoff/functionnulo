5be4a49e35028998053bdda517e39c1775c829c9
Michailoff <mateus.impact@hotmail.com>
2019-08-15 19:11:36 -0300

    nulo

---
+++ js/index.js
@@ -0,0 +1,12 @@
+var number = parseInt(prompt("Digite um valor"));
+
+function nule(number){
+
+	if(number != 0) {
+       return document.body.append("nao é nulo");
+   }
+     else(number == 0)
+       return document.body.append("Nulo");
+    }
+
+nule(number)
